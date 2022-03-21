63597a4062943b9f4508cb24319728dbf3a473bf
andrey-korchevniy <91483336+andrey-korchevniy@users.noreply.github.com>
2022-03-21 14:50:45 +0200

    hw10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,8 @@
+const formInput = document.querySelector('input');
+
+formInput.addEventListener('')
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
