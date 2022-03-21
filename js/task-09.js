ef2cabb820ccc7341ab86aa4f4f59bab860e48b2
andrey-korchevniy <91483336+andrey-korchevniy@users.noreply.github.com>
2022-03-21 12:48:49 +0200

    hw9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const colorWidget = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+const bodyInfo = document.querySelector('body');
+
+changeColorBtn.addEventListener('click', changeColor);
+
+function changeColor() {
+  bodyInfo.style.backgroundColor= getRandomHexColor();
+  colorWidget.textContent = bodyInfo.style.backgroundColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+ 
