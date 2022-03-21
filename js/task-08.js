a01dbc7f9566ad8d89a3e73d931f6f73887d5f4b
andrey-korchevniy <91483336+andrey-korchevniy@users.noreply.github.com>
2022-03-21 11:41:50 +0200

    hw8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener('submit', onSubmit);
+
+function onSubmit() {
+    event.preventDefault();
+    const { elements: { email, password } } = event.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        alert('Все поля должны быть заполнены')
+    }
+    else {
+        const formForSend = {
+            email: email.value,
+            password: password.value
+        };
+        console.log(formForSend);
+        event.currentTarget.reset();
+    }
+
+}
 
